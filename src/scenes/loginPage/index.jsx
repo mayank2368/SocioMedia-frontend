857023565dbf38c608f27f837d252210e25f2d75
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-04-26 15:12:45 +0200

    Modified the loginPage UI along with the login and registeration form creation, validation  and routing

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -1,13 +1,37 @@
-import { Box } from "@mui/material";
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 import React from "react";
-import Navbar from "scenes/navbar";
 
-const loginPage = () => {
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px");
   return (
     <Box>
-      <Navbar />
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          SocioMedia
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to SocioMedia, the Social Media for SocioPaths!!!
+        </Typography>
+        <Form />
+      </Box>
     </Box>
   );
 };
 
-export default loginPage;
+export default LoginPage;
